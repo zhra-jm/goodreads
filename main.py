f22710af9baa7b8a4f7f34c0ff79cfe8b17e74a4
Your Name <zahrajam1999@gmail.com>
2021-05-09 18:07:08 +0430

    step5-done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from reports import show_users, show_books
 from importer import UserImporter, BookImporter, BookAuthorImporter, ShelfImporter, BookShelfImporter, AuthorImporter
 from models import database, User, Book, Author, Shelf, BookShelf, BookAuthor, BookTranslator, UserAuthorRelation, \
     UserRelation
+from peewee import fn
 
 
 def create_tables():
@@ -49,12 +50,35 @@ def show_user_data(username='hosein', password='654321'):
     # )
 
 
+def show_book_rates():
+    query = BookShelf.select(
+        BookShelf.book,
+        fn.SUM(BookShelf.rate).alias('rate_sum'),
+        fn.COUNT(BookShelf.rate).alias('rate_count')
+    ).group_by(BookShelf.book)
+    for q in query:
+        print(q.book_id, q.rate_sum / q.rate_count)
+
+
+def show_book_shelves():
+    query = BookShelf.select(
+        BookShelf.user,
+        BookShelf.shelf,
+        fn.count(BookShelf.book).alias('book_count')
+    ).group_by(BookShelf.shelf)
+
+    for q in query:
+        print(q.user.username, q.shelf.name, q.book_count)
+
+
 if __name__ == '__main__':
     # create_tables()
     # load_data()
     # show_data()
     # show_user_data('hosein', '654321')
     # show_user_data('zhra')
-    show_user_data()
+    # show_user_data()
     # bs = BookShelf.get_by_id(2)
     # bs.change_to_read()
+    show_book_rates()
+    # show_book_shelves()
